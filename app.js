5fe781ee7cc523eecc1fb84b05c28cec37bd3e81
Robert Ellis <robertellis.rob@gmail.com>
2015-09-26 18:13:19 -0600

    Step 1 Complete: Set-up is connected and working

---
+++ app.js
@@ -0,0 +1,8 @@
+var app = angular.module('racingFrogs', []);
+app.controller('MainController', MainController);
+//No need to change anything above this line.
+
+function MainController() {
+    var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
+
+}
