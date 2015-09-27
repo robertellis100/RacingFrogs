bfe52590bc00ed82d5eb0f25d193d4e6f4293c95
Robert Ellis <robertellis.rob@gmail.com>
2015-09-26 20:24:05 -0600

    Functional per instructions from http://bcw.azurewebsites.net/#/objects. Added currency filter as well as "$" in alert messages.

--- app.js
+++ app.js
@@ -4,33 +4,42 @@ app.controller('MainController', MainController);
 
 function MainController() {
     var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
+    //sounds good
+    vm.joe = new Guy("Joe", 100)
+    vm.bob = new Guy("Bob", 150)
+    vm.bank = 200;
+    
     function Guy(name, startingCash) {
-    //insert code here
-        this.name=name;
-        this.cash=startingCash;
+        this.name = name;
+        this.cash = startingCash;
         this.giveCash = function(amount) {
-            if (this.cash>=amount) { 
-                // this.giveCash=function (person,amount){
-                    // this.cash-=amount;
-                    // person.receiveCash(amount);
-                
-                //fill in the if statement. Does this guy have enough money to give?
-                //subtract from the guys cash the amount requested, don't forget 'this'
-                this.cash-=amount;
-                return amount; //return the amount given.
+            if (amount <= this.cash && amount > 0) {
+                this.cash = this.cash - amount;
+                return amount;
             } else {
-                alert("I don't have enough cash to give you");
-                return 0;
-            }   
-        };
-        this.receiveCash=function (amount){
-            if (Math.sign(amount)>=0) {
-                this.cash+=amount;
-            }
-            else{
-                alert("I don't accept IOUs. You filthy scoundrel");
+                alert("I don't have enough cash to give you $" + amount + ". " + this.name + " says...");
                 return 0;
             }
         };
-    };
-}   
+        this.receiveCash = function(amount) {
+            if (amount > 0) {
+                this.cash = this.cash + amount;
+                return amount;
+            } else {
+                alert("No IOUs.  $"+ amount + " isn't an amount I'll take " + this.name + " says...");
+                return 0;
+            }
+        }
+    }
+    vm.giveMoneyToJoe = function() {
+        if (vm.bank >= 10) {
+            vm.bank -= vm.joe.receiveCash(10)
+        } else {
+            alert("The bank is out of money.")
+        }
+    }
+
+    vm.receiveMoneyFromBob = function() {
+        vm.bank += vm.bob.giveCash(5)
+    }
+}
